5a57a6f3588dc118203abf53b77d55804326ef8e
miguelrey0415 <101655447+miguelrey0415@users.noreply.github.com>
2022-03-15 09:32:53 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+const texts = document.querySelector('.texts');
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+
+recognition.addEventListener('result', (e)=>{
+  texts.appendChild(p);
+  const text = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+
+  p.innerText = text;
+  if(e.results[0].isFinal){
+    if (text.includes('hola')) {
+      p = document.createElement('p');
+      p.classList.add('replay');
+      p.innerText = 'hola';
+      texts.appendChild(p)
+    }
+    if (text.includes("me llamo miguel") || text.includes('me llamo miguel')) {
+      p = document.createElement('p');
+      p.classList.add('replay');
+      p.innerText = 'My Name is Cifar';
+      texts.appendChild(p)
+    }
+    if (text.includes('reproduce mi video favorito')) {
+      p = document.createElement('p');
+      p.classList.add('replay');
+      p.innerText = 'reproduce mi video favorito';
+      texts.appendChild(p)
+      console.log('opening youtube')
+      window.open('https://www.youtube.com/watch?v=9BMwcO6_hyA')
+    }
+    p = document.createElement('p');
+  }
+});
+
+
+recognition.addEventListener('end', ()=>{
+  recognition.start();
+})
+
+recognition.start();
